1fb1823987b041b586daeabcb313ea8763bebea3
ziax <ziaxdk@gmail.com>
2014-04-05 20:35:03 +0200

    fix

--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,9 @@ angular.module('detkoeberjeg.services', [])
 .service('User', function($window) {
   return {
     get: function() {
+      alert('get');
       var json = $window.localStorage.getItem('user');
+      alert(json);
       if (!json) return;
       return JSON.parse(json);
     },
