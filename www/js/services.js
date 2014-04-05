ff15962c7432273bb328ea84eb2a81c5926ec8f6
ziax <ziaxdk@gmail.com>
2014-04-05 20:38:40 +0200

    fix

--- www/js/services.js
+++ www/js/services.js
@@ -7,7 +7,9 @@ angular.module('detkoeberjeg.services', [])
       var json = $window.localStorage.getItem('user');
       alert(json);
       if (!json) return;
-      return JSON.parse(json);
+      var res = JSON.parse(json);
+      alert(res);
+      return res;
     },
     store: function(user) {
       $window.localStorage.setItem('user', JSON.stringify(user));
